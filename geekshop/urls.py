f47ef0315eadaf875117f5426ff596deb06fd691
navsex <navsex@list.ru>
2021-02-28 21:00:29 +0300

    completed except 6, 7

--- geekshop/urls.py
+++ geekshop/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -27,6 +27,11 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', index, name='index'),
     path('products/', products, name='products'),
+
+    # path('products/', include('mainapp.urls', namespace='products')),
+
+    # path('login/', login, name='login'),
+    # path('profile/', profile, name='profile'),
 ]
 
 if settings.DEBUG:
---
+++ mainapp/urls.py
@@ -0,0 +1,10 @@
+# from django.urls import path
+#
+# import mainapp.views as mainapp
+#
+#
+# urlpatterns = [
+#     path('', mainapp.products),
+#     # path('<int:pk>/', mainapp.products, namespace='category'),
+# ]
+
