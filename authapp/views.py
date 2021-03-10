fb0f1eee7d523bf780c03cedbd4b3ee1de092cbb
navsex <navsex@list.ru>
2021-03-10 23:47:41 +0300

    authapp >> login + register = base.html

--- authapp/views.py
+++ authapp/views.py
@@ -2,12 +2,12 @@ from django.shortcuts import render
 
 def login(request):
     context = {
-
+        'title': 'GeekShop - Авторизация',
     }
     return render(request, 'authapp/login.html', context)
 
 def register(request):
     context = {
-
+        'title': 'GeekShop - Регистрация',
     }
     return render(request, 'authapp/register.html', context)
